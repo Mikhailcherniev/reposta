8631af76ecf70c89227851d3338a0f0778d8f52f
Micael_Henrique <133661765+Mikhailcherniev@users.noreply.github.com>
2023-08-08 13:25:31 -0300

    Initial commit

---
+++ java.js
@@ -0,0 +1,16 @@
+function mostrarMenu(){
+    var menu = documente.getElementById("nav-header");
+    var icone = document.getElementById("icone");
+
+    if(getComputedStyle(menu).display=='none'){
+        menu.style.display='flex';
+        icone.classList.remove("fa-bars");
+        icone.classList.add("fa-time");
+
+    }else{
+        menu.style.display="none";
+        icone.classList.remove("fa-times");
+        icone.classList.add("fa-bars")
+    }
+
+}
